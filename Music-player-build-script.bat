6d67b0dbc928c4c56007765de1d759124bec7d76
Fahad <rjfahad44@gmail.com>
2025-04-13 16:58:12 +0600

    update Readme file.

---
+++ Music-player-build-script.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Building Music Player Installer...
+"C:\Program Files (x86)\Inno Setup 6\ISCC.exe" music-player-setup.iss
+if %ERRORLEVEL%==0 (
+    echo Installer created at: Output\MyMusicSetup.exe
+) else (
+    echo Error: Failed to create installer
+    exit /b %ERRORLEVEL%
+)
